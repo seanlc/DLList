913fafa6c2927c39e5963c0c8eeba149276f357c
Sean <seanlcarstensen@gmail.com>
2017-08-14 22:59:04 -0700

    implemented ctor, dtor, part of insert(), getLength(), isEmpty(), print()

--- DLList.h
+++ DLList.h
@@ -12,15 +12,74 @@ struct Node
 class DLList
 {
   public:
-    DLList();
-    ~DLList();
-    void insertNum(int n);
+    DLList()
+    : head (nullptr), tail (nullptr), numNodes (0)
+    {}
+    ~DLList()
+    {
+        Node * tmp = head;
+	while(tmp != nullptr)
+	{
+	    head = head->next;
+	    delete tmp;
+	    tmp = head;
+	}
+    }
+    void insertNum(int n)
+    {
+	Node * trail = nullptr;
+	Node * trav = nullptr;
+	Node * newNode = new Node;
+	newNode->data = n;
+	newNode->next = nullptr;
+	newNode->last = nullptr;
+
+	// empty list case
+        if(head == nullptr && tail == nullptr)
+	{
+	    head = newNode;
+	    tail = newNode;
+	}
+	else
+	{
+
+	    // find insertion spot
+	    for(trav = head; newNode->data > trav->data; trav = trav->next)
+            {
+	        trail = trav;
+	    }
+
+	    if(trav == head)
+	    {
+	        trav->last = newNode;
+		newNode->next = trav;
+		head = newNode;
+	    }
+	}
+
+	++numNodes;
+
+    }
     void deleteNum(int n);
     bool findNum(int n);
-    int getLength();
+    int getLength()
+    {
+        return numNodes;
+    }
     void makeEmpty();
-    bool isEmpty();
-    void print();
+    bool isEmpty()
+    {
+        return numNodes == 0;
+    }
+    void print()
+    {
+        Node * trav = head;
+	while(trav != nullptr)
+	{
+	    cout << "node with val " << trav->data << endl;
+	    trav = trav->next;
+	}
+    }
   private:
     Node * head;
     Node * tail;
--- main.cpp
+++ main.cpp
@@ -8,8 +8,16 @@ void test_constr_destr()
     DLList myList;
 }
 
+void test_insert()
+{
+    DLList myList;
+    myList.insertNum(0);
+    myList.print();
+}
+
 int main()
 {
-    test_constr_destr();
+//    test_constr_destr();
+    test_insert();
     return 0;
 }
