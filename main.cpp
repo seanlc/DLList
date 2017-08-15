f8032545712cd4b0317511462018e390ffb49f83
Sean <seanlcarstensen@gmail.com>
2017-08-14 23:05:19 -0700

    wrote test harnesses for getLength() and isEmpty()

--- main.cpp
+++ main.cpp
@@ -15,9 +15,28 @@ void test_insert()
     myList.print();
 }
 
+void test_isEmpty()
+{
+    DLList myList;
+    cout << "isEmpty() returns " << myList.isEmpty() << " for empty list" << endl;
+    myList.insertNum(0);
+    cout << "isEmpty() returns " << myList.isEmpty() << " for nonempty list" << endl;
+}
+
+void test_isLength()
+{
+    DLList myList;
+    cout << "length of empty list: " << myList.getLength() << endl;
+    myList.insertNum(0);
+    cout << "length of nonempty list: " << myList.getLength() << endl;
+
+}
+
 int main()
 {
 //    test_constr_destr();
-    test_insert();
+//    test_insert();
+//    test_isEmpty();
+    test_isLength();
     return 0;
 }
